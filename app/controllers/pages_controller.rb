63ceb472fca94a1ecf71f5e4ba53715c90ada472
Lea Schumacher <miss6muffet@gmail.com>
2020-12-19 16:51:04 +0100

    created controller, routes, views

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+    def home
+    end
+
+    def about
+    end
+
+    def christmas_story_venardou
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # LANDING PAGE
+  root to: 'pages#home'
+  # CHRISTMAS STORY PAGE
+  get '/christmas_story_venardou', to: 'pages#christmas_story_venardou'
+  # ABOUT PAGE
+  get '/about', to: 'pages#about'
+  # ERROR PAGES
+  get '/404', to: 'errors#not_found'
+  get '/422', to: 'errors#unacceptable'
+  get '/500', to: 'errors#internal_error'
 end
