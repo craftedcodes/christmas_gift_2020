847aaa96fb29851ab774442da20ccfd41a6b29aa
Lea Schumacher <miss6muffet@gmail.com>
2020-12-23 03:17:24 +0100

    raising responsiveness and adding scroll animation

---
+++ app/javascript/scripts/hidden.js
@@ -0,0 +1,15 @@
+const hideMouse = () => {
+  document.getElementById("hidden").classList.remove("hide");
+};
+
+const mediaQuery = window.matchMedia("(max-width:380px)");
+
+const scrollMouse = () => {
+  // checking if the id exists on the element makes it possible to choose on which page it's goiing to be loaded
+  const hide = document.getElementById("hidden");
+  if (mediaQuery.matches) {
+    hideMouse;
+  }
+};
+
+export { scrollMouse };
